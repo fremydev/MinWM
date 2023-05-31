ebe25a57e7fec50b8f3b3c0926750bcc17f3c922
fremydev <fremy.dev@gmail.com>
2023-05-31 17:54:43 -0300

    add basic X11 window manager loop

--- main.c
+++ main.c
@@ -3,5 +3,14 @@
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 int main () {
+	Display *dpy;
+	XEvent ev;
+
+	if(!(dpy = XOpenDisplay(0x0))) return 1;
+
+	for(;;) {
+		XNextEvent(dpy, &ev);
+	}
+
 	return 0;
 }
